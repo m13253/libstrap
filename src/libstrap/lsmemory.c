697b7d86fbd1d927b420e1f9a9afbf0918a3bf35
Star Brilliant <m13253@hotmail.com>
2014-07-30 09:45:19 +0800

    lsmemory.c

--- src/libstrap/lsmemory.c
+++ src/libstrap/lsmemory.c
@@ -62,3 +62,66 @@ void *ls_malloc_fail(size_t size) {
         return NULL;
     }
 }
+
+void *ls_malloc0_fail(size_t size) {
+    if(size != 0) {
+        return memory_functions.calloc_func(1, size);
+    } else  {
+        return NULL;
+    }
+}
+
+void *ls_calloc(size_t nmemb, size_t size) {
+    if(nmemb != 0 && size != 0) {
+        void *res = ls_calloc_fail(nmemb, size);
+        if(res) {
+            return res;
+        } else {
+            abort();
+        }
+    } else {
+        return NULL;
+    }
+}
+
+void *ls_calloc_fail(size_t nmemb, size_t size) {
+    if(nmemb != 0 && size != 0) {
+        return memory_functions.calloc(nmemb, size);
+    } else {
+        return NULL;
+    }
+}
+
+void *ls_realloc(void *ptr, size_t newsize) {
+    if(newsize != 0) {
+        *res = ls_realloc_fail(ptr, newsize);
+        if(res) {
+            return res;
+        } else {
+            abort();
+        }
+    } else {
+        ls_free(ptr);
+        return NULL;
+    }
+}
+
+void *ls_realloc_fail(void *ptr, size_t newsize) {
+    if(newsize != 0) {
+        if(ptr) {
+            return memory_functions.realloc(ptr, newsize);
+        } else {
+            return memory_functions.malloc(newsize);
+        }
+    } else {
+        ls_free(ptr);
+        return NULL;
+    }
+}
+
+void *ls_free(void *ptr) {
+    if(ptr) {
+        memory_functions.free(ptr);
+    }
+    return NULL;
+}
